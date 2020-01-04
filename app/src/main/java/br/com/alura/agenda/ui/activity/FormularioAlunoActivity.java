a7c216256378ab6c7e1e82456cee9b785b4ebf22
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-01-04 20:17:39 -0300

    Projeto do curso 1 completo

---
+++ app/src/main/java/br/com/alura/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,76 @@
+package br.com.alura.agenda.ui.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.dao.AlunoDao;
+import br.com.alura.agenda.model.Aluno;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Novo aluno";
+    private TextView campoNome;
+    private TextView campoTelefone;
+    private TextView campoEmail;
+    private final AlunoDao dao = new AlunoDao();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+
+        setTitle(TITULO_APPBAR);
+
+        //inicializa os campos do formulario
+        inicializacaoDosCampos();
+        //cria mascara para o campo de telefone
+        criandoMascaraParaTelefone();
+        //configura o botao que salva os dados do aluno
+        configuraBotaoSalvar();
+
+    }
+
+    private void configuraBotaoSalvar() {
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Aluno alunoCriado = criaAluno();
+                salvar(alunoCriado);
+            }
+        });
+    }
+
+    private void criandoMascaraParaTelefone() {
+        SimpleMaskFormatter smf = new SimpleMaskFormatter("(NN) NNNN-NNNN");
+        MaskTextWatcher mtw = new MaskTextWatcher(campoTelefone, smf);
+        campoTelefone.addTextChangedListener(mtw);
+    }
+
+    private void inicializacaoDosCampos() {
+        campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+    }
+
+    private void salvar(Aluno aluno) {
+        dao.salva(aluno);
+        finish();
+    }
+
+    private Aluno criaAluno() {
+        String nome = campoNome.getText().toString();
+        String telefone = campoTelefone.getText().toString();
+        String email = campoEmail.getText().toString();
+        return new Aluno(nome, telefone, email);
+    }
+}
